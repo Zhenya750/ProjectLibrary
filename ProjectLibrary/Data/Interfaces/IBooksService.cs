b426247888fdd0eced5a0487492bddc41bb40b8a
zhenya750 <zhenya750bus@mail.ru>
2021-09-01 00:34:33 +0300

    added interface for db

---
+++ ProjectLibrary/Data/Interfaces/IBooksService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Data.Interfaces
+{
+    public interface IBooksService
+    {
+        IEnumerable<Book> GetAll();
+        IEnumerable<Book> GetByGenre(string genre);
+        void Add(Book book);
+        void Delete(Book book);
+    }
+}
