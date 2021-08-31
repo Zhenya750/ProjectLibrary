d27e2dbe0b2dccd0497adcc218dcb87e32457386
zhenya750 <zhenya750bus@mail.ru>
2021-09-01 00:34:15 +0300

    added init books controller

---
+++ ProjectLibrary/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        // GET: api/books
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/books/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/books
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/books
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/books/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
