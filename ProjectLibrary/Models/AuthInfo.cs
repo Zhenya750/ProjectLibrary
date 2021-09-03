4711c7d7abb6132cea0223c410ff941a60d0e38a
zhenya750 <zhenya750bus@mail.ru>
2021-09-04 00:24:59 +0300

    added auth model for user

---
+++ ProjectLibrary/Models/AuthInfo.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using ProjectLibrary.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Models
+{
+    public class AuthInfo
+    {
+        [BsonId]
+        [Required]
+        [MinLength(3, ErrorMessage = "Login must contain at least 3 symbols")]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Password must contain at least 3 symbols")]
+        public string Password { get; set; }
+    }
+}
