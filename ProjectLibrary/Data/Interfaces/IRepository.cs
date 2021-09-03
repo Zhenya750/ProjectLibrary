213b3ddaf3644164b98fe252a5adc4017b1d12e4
zhenya750 <zhenya750bus@mail.ru>
2021-09-04 00:26:13 +0300

    added generic repository for base model

---
+++ ProjectLibrary/Data/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProjectLibrary.Models;
+using ProjectLibrary.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectLibrary.Data.Interfaces
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        IEnumerable<T> GetAll();
+        T Get(string id);
+        T Create(T model);
+        void Update(T model);
+        void Delete(string id);
+    }
+}
